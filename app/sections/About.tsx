6b97b6b59012a6b0c2a4df41c5427ae20760ac30
tn-py <tirso.an08@gmail.com>
2024-11-30 00:36:03 -0500

    fixed indentation issue

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -10,25 +10,25 @@ const About = () => {
             id="about"
         >
             <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
-                    <AnimatedTitle
-                        text={"Web Operations Supervisor, UHS Hardware"}
-                        className={
-                            "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
-                        }
-                        wordSpace={"mr-[14px]"}
-                        charSpace={"mr-[0.001em]"}
-                    />
-                    <AnimatedTitle
-                        text={"E-commerce & Web Development"}
-                        className={
-                            "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
-                        }
-                        wordSpace={"mr-[14px]"}
-                        charSpace={"mr-[0.001em]"}
-                    />
+                <AnimatedTitle
+                    text={"Web Operations Supervisor, UHS Hardware"}
+                    className={
+                        "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
+                    }
+                    wordSpace={"mr-[14px]"}
+                    charSpace={"mr-[0.001em]"}
+                />
+                <AnimatedTitle
+                    text={"E-commerce & Web Development"}
+                    className={
+                        "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
+                    }
+                    wordSpace={"mr-[14px]"}
+                    charSpace={"mr-[0.001em]"}
+                />
 
                 <div className="mx-auto flex w-[100%] flex-col lg:max-w-[1200px] lg:flex-row lg:gap-20">
-                    <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium  leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16  lg:max-w-[90%] lg:text-[24px] ">
+                    <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16 lg:max-w-[90%] lg:text-[24px]">
                         <AnimatedBody text="I'm an aspiring Full-Stack Web Developer based in Mexico City. I have extensive experience in Shopify theme development, specializing in creating seamless digital solutions that enhance user experience and operational efficiency. I'm proficient in building and optimizing Shopify themes, pages, scripts, and automations. Utilizing tools like Shopify Flow and Matrixify, I perform mass uploads and updates to streamline processes and reduce operational costs." />
 
                         <AnimatedBody
